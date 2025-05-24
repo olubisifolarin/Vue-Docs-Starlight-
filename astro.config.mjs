51e6052b471a1c474c6794aa3f8b875826443455
olubisifolarin <hollubissy4real10@gmail.com>
2025-05-24 21:54:00 +0100

    essentails added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://olubisifolarin.github.io',
+	site: 'https://olubisifolarin.github.io/Vue-Docs-Starlight-',
   	base: 'Vue-Docs-Starlight-',
 	integrations: [
 		starlight({
